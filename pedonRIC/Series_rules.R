ffc9932da43006318d80dde79002feac3377c51b
Dave White <dave-white2@users.noreply.github.com>
2022-09-22 15:36:22 -0600

    Add files via upload

---
+++ pedonRIC/Series_rules.R
@@ -0,0 +1,16 @@
+ghr <- list(
+  
+  #### COPY AND PASTE n from GenHznLab
+  n = c("A", 
+        "Ck")
+  
+  ,
+  
+  ### COPY AND PASTE p from GenHznLab
+  p = c("A|Ck1",
+        "^Ck$|Ck2|Ck3|Ck4")
+  
+  
+)
+
+#### After copied save file as:::    Series_rules.R  <----- make sure to change the series name
---
+++ pedonRIC/adelino.R
@@ -0,0 +1,18 @@
+ghr <- list(
+  
+  #### COPY AND PASTE n from GenHznLab
+  n = c("A", 
+        "Bw",
+        "C")
+  
+  ,
+  
+  ### COPY AND PASTE p from GenHznLab
+  p = c("A",
+        "B|^C",
+        "^2C$")
+  
+  
+)
+
+#### After copied save file as:::    Series_rules.R  <----- make sure to change the series name
